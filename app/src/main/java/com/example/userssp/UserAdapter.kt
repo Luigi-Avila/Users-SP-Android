8bb0be9d1740519aadda17bb2cc9550a91807d08
Luigi <l.avila.uvg@gmail.com>
2023-05-04 17:41:43 -0600

    feat: User adapter with members completed

--- app/src/main/java/com/example/userssp/UserAdapter.kt
+++ app/src/main/java/com/example/userssp/UserAdapter.kt
@@ -1,5 +1,7 @@
 package com.example.userssp
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -7,17 +9,23 @@ import com.example.userssp.databinding.ItemUserBinding
 
 class UserAdapter(private val users: List<User>): RecyclerView.Adapter<UserAdapter.ViewHolder>() {
 
+    private lateinit var context: Context
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        context = parent.context
+        val view = LayoutInflater.from(context).inflate(R.layout.item_user, parent, false)
+        return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val user = users[position]
+        with(holder){
+            binding.tvOrder.text = user.id.toString()
+            binding.tvName.text = user.name
+        }
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = users.size
 
     inner class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
         val binding = ItemUserBinding.bind(view)
