a594646f6a41935e2376e369c0d38a19646c04b6
Luigi <l.avila.uvg@gmail.com>
2023-05-04 17:54:57 -0600

    feat: recyclerview and binding on activity main page added

--- app/src/main/java/com/example/userssp/MainActivity.kt
+++ app/src/main/java/com/example/userssp/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.userssp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.userssp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var userAdapter: UserAdapter
+    private lateinit var linearLayout: RecyclerView.LayoutManager
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        userAdapter = UserAdapter(mutableListOf())
+        linearLayout = LinearLayoutManager(this)
+
+        binding.recyclerView.apply {
+            layoutManager = linearLayout
+            adapter = userAdapter
+        }
+
     }
 }
