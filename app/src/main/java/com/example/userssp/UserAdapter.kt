1f04a20ffbb8b62512508830e4a78567b06f51c2
Luigi <l.avila.uvg@gmail.com>
2023-05-09 15:47:22 -0600

    feat: show user image

--- app/src/main/java/com/example/userssp/UserAdapter.kt
+++ app/src/main/java/com/example/userssp/UserAdapter.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.example.userssp.databinding.ItemUserBinding
 
 class UserAdapter(private val users: List<User>): RecyclerView.Adapter<UserAdapter.ViewHolder>() {
@@ -20,8 +22,14 @@ class UserAdapter(private val users: List<User>): RecyclerView.Adapter<UserAdapt
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val user = users[position]
         with(holder){
-            binding.tvOrder.text = user.id.toString()
+            binding.tvOrder.text = (position + 1).toString()
             binding.tvName.text = user.name
+            Glide.with(context)
+                .load(user.url)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .centerCrop()
+                .circleCrop()
+                .into(binding.imgPhoto)
         }
     }
 
